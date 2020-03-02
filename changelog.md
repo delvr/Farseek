31f786bb2ed413d096b6c91e2b538b9265013a12
delvr <delver.delves@gmail.com>
2020-03-01 22:07:20 -0800

    v1.12-2.5

--- changelog.md
+++ changelog.md
@@ -1,14 +1,3 @@
-- Farseek structure generators now use world's own chunk generator for structure-placement chunk generation, instead of creating a copy. This should improve Streams performance and mod compatibility, but may break some edge cases (please report).
-- As a result of the above, possibly fixed infinite Streams generation with certain combinations of mods (to be confirmed). Relevant issues include
-    [Farseek #44](https://github.com/delvr/Farseek/issues/44),
-    [Farseek #48](https://github.com/delvr/Farseek/issues/48),
-    [Farseek #51](https://github.com/delvr/Farseek/issues/51),
-    [Streams #74](https://github.com/delvr/Streams/issues/74),
-    [Streams #78](https://github.com/delvr/Streams/issues/78),
-    [Streams #83](https://github.com/delvr/Streams/issues/83),
-    [Streams #86](https://github.com/delvr/Streams/issues/85),
-    [Streams #86](https://github.com/delvr/Streams/issues/86), and
-    [Streams #90](https://github.com/delvr/Streams/issues/90).
-- As a consequence of the above, had to drop support for OpenTerrainGenerator versions earlier than v8 
-  (game won't crash with older versions, but no Farseek features such as Streams will generate in those).
-- Fixed [Streams crash with SpongeForge 7.1.7 and newer](https://github.com/delvr/Farseek/issues/49).
+- Fixed [Streams mod generating water rivers in the Nether](https://github.com/delvr/Streams/issues/97).
+- Fixed [crash with recent versions of SpongeForge](https://github.com/delvr/Farseek/issues/55) (tested with release versions 7.1.9 and 7.1.10).
+- Possibly fixed [Streams crash with certain mod combinations](https://github.com/delvr/Farseek/issues/56) (to be confirmed).
--- src/main/scala/farseek/world/gen/structure/StructureGenerationChunkProvider.scala
+++ src/main/scala/farseek/world/gen/structure/StructureGenerationChunkProvider.scala
@@ -1,9 +1,21 @@
 package farseek.world.gen.structure
 
-import net.minecraft.world.{World, WorldServer}
+import com.pg85.otg.OTG
+import com.pg85.otg.forge._
+import com.pg85.otg.forge.generator.OTGChunkGenerator
+import farseek.util.ImplicitConversions._
+import farseek.util.Reflection._
+import farseek.util._
+import java.lang.reflect.Method
+import net.minecraft.world._
+import net.minecraft.world.gen.IChunkGenerator
+import net.minecraft.world.gen.structure.MapGenStructure
+import net.minecraftforge.common.MinecraftForge._
+import net.minecraftforge.event.world.WorldEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import scala.collection.mutable
 
-/** A chunk provider for [[Structure]]s that creates chunks with only raw terrain and _without_ structures.
+/** A chunk provider for [[Structure]]s that contains a copy of a world's chunk generator and creates chunks _without_ structures.
   *
   * These chunks are used for structures being generated to query the terrain in their range without triggering recursion by generating more of themselves.
   * This enables Farseek structures to be terrain-aware in a way that vanilla structures cannot be, but also means that structures cannot
@@ -12,14 +24,58 @@ import scala.collection.mutable
   *
   * @author delvr
   */
-case class StructureGenerationChunkProvider(world: World, generator: (Int, Int) => Unit)
+class StructureGenerationChunkProvider(world: WorldServer) extends Logging {
 
-/** Companion object for [[StructureGenerationChunkProvider]]s, that maintains a mapping of [[World]]s to `StructureGenerationChunkProvider`s.
+    val worldProvider = world.provider
+
+    debug(s"Creating structure generation chunk provider for world $worldProvider")
+
+    val generator: IChunkGenerator =
+      if(world.getWorldType.getClass.getSimpleName == "OTGWorldType")
+        new OTGChunkGenerator(OTG.getEngine.asInstanceOf[ForgeEngine].getWorld(world))
+      else classOf[WorldServer].declaredMethod("impl$updateWorldGenerator").map(
+        createSpongeGenerator).getOrElse(worldProvider.createChunkGenerator)
+
+    private def createSpongeGenerator(creatorMethod: Method): IChunkGenerator = {
+        val newBaseGenerator = worldProvider.createChunkGenerator
+        val currentSpongeGenerator = world.getChunkProvider.chunkGenerator
+        world.getChunkProvider.chunkGenerator = newBaseGenerator
+        creatorMethod(world)
+        val newSpongeGenerator = world.getChunkProvider.chunkGenerator
+        world.getChunkProvider.chunkGenerator = currentSpongeGenerator
+        newSpongeGenerator
+    }
+
+    debug("World generator: " + world.getChunkProvider.chunkGenerator)
+    debug("Farseek generator: " + generator)
+
+    classFieldValues[MapGenStructure](generator).foreach(_.range = -1) // Disable structure generators
+
+    EVENT_BUS.register(this)
+
+    def generateChunk(xChunk: Int, zChunk: Int) = {
+        val chunk = generator.generateChunk(xChunk, zChunk)
+        chunk.setTerrainPopulated(true)
+        chunk
+    }
+
+    @SubscribeEvent def onWorldUnload(event: WorldEvent.Unload) {
+        if(event.getWorld.provider == this.worldProvider) {
+            debug(s"Removing structure generation chunk provider for world $worldProvider")
+            EVENT_BUS.unregister(this)
+            StructureGenerationChunkProvider.remove(event.getWorld.provider)
+        }
+    }
+}
+
+/** Companion object for [[StructureGenerationChunkProvider]]s, that maintains a mapping of [[WorldProvider]]s to `StructureGenerationChunkProvider`s.
   * @author delvr
   */
-object StructureGenerationChunkProvider {
+object StructureGenerationChunkProvider extends Logging {
 
-    val providers: mutable.Map[World, StructureGenerationChunkProvider] = mutable.Map[World, StructureGenerationChunkProvider]()
+    private val providers = mutable.Map[WorldProvider, StructureGenerationChunkProvider]()
 
-    def apply(world: WorldServer): StructureGenerationChunkProvider = providers(world)
+    def apply(world: WorldServer) = providers.getOrElseUpdate(world, new StructureGenerationChunkProvider(world))
+
+    def remove(worldProvider: WorldProvider) = providers.remove(worldProvider)
 }
